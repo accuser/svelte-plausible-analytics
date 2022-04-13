c660904be32f7f6dbdfc8c3603e034799db23a78
Matthew Gibbons <mhgibbons@me.com>
2022-04-13 08:45:20 +0100

    Google Analytics like

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,4 +7,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<button on:click={e => login('click', 'blop')}>Login</button>
+<button on:click={e => login('click')}>Login</button>
