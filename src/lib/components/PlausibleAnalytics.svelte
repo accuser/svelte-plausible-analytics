7c56ae6b4a104abfe6d66bfed05d6763ea4ff00b
Matthew Gibbons <mhgibbons@me.com>
2024-10-01 08:31:46 +0000

    Support all extensions (expect manual)

--- src/lib/components/PlausibleAnalytics.svelte
+++ src/lib/components/PlausibleAnalytics.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import events from '$lib/events/events.svelte.js';
+	import events from '$lib/components/events/events.svelte.js';
 	import { type PlausibleOptions } from 'plausible-tracker';
 
 	type Props = PlausibleOptions & {
@@ -18,8 +18,16 @@
 		enabled?: boolean;
 
 		/**
+		 * Enable tracking of custom properties.
+		 * @see {@link https://plausible.io/docs/custom-props/introduction}
+		 *
+		 * @defaultValue `undefined`
+		 */
+		event?: { [key: string]: string };
+
+		/**
 		 * Automatically track file downloads.
-		 * (Requires manual goal configuration on Plausible.)
+		 * @see {@link https://plausible.io/docs/file-downloads-tracking}
 		 *
 		 * @defaultValue `false`
 		 */
@@ -41,7 +49,32 @@
 		outboundLinks?: boolean;
 
 		/**
+		 * Enable ecommerce revenue and attribution tracking.
+		 * @see{@link https://plausible.io/docs/ecommerce-revenue-tracking}
+		 *
+		 * @defaultValue `false`
+		 */
+		revenue?: boolean;
+
+		/**
+		 * Enable tracking of custom events.
+		 * @see {@link https://plausible.io/docs/custom-event-goals}
+		 *
+		 * @defaultValue `false`
+		 */
+		trackCustomEvents?: boolean;
+
+		/**
+		 * Enable automatic tracking of 404 error pages.
+		 * @see {@link https://plausible.io/docs/error-pages-tracking-404}
+		 *
+		 * @defaultValue `false`
+		 */
+		trackErrorPages?: boolean;
+
+		/**
 		 * Enable automatic tracking of outbound links.
+		 * @see {@link https://plausible.io/docs/outbound-link-click-tracking}
 		 *
 		 * @defaultValue `false`
 		 */
@@ -55,28 +88,45 @@
 		hash = false,
 		local = enabled && MODE === 'development',
 		outboundLinks = false,
-		apiHost = 'https://plausible.io',
+		apiHost,
 		compat = false,
 		domain = typeof window !== 'undefined' ? window.location.hostname : undefined,
+		event,
 		fileDownloads = false,
 		hashMode = hash,
+		revenue = false,
+		trackErrorPages = false,
+		trackCustomEvents = false,
 		trackLocalhost = local,
 		trackOutboundLinks = outboundLinks
 	}: Props = $props();
 
-	let api = $derived(`${apiHost}/api/event`);
+	let api = $derived(apiHost ? `${apiHost}/api/event` : undefined);
 	let src = $derived(
 		[
-			`${apiHost}/js/script`,
+			`${apiHost ?? 'https://plausible.io'}/js/script`,
 			compat ? 'compat' : undefined,
 			fileDownloads ? 'file-downloads' : undefined,
 			hashMode ? 'hash' : undefined,
+			revenue ? 'revenue' : undefined,
+			trackCustomEvents ? 'tagged-events' : undefined,
+			event ? 'pageview-props' : undefined,
 			trackLocalhost ? 'local' : undefined,
 			trackOutboundLinks ? 'outbound-links' : undefined,
+			trackCustomEvents ? 'tagged-events' : undefined,
 			'js'
 		]
 			.filter(Boolean)
 			.join('.')
+	);
+
+	let pageviewProps = $derived.by(() =>
+		event
+			? Object.entries(event).reduce(
+					(acc, [key, value]) => Object.assign(acc, { [`event-${key}`]: value }),
+					{} as { [key: string]: string }
+				)
+			: undefined
 	);
 
 	$effect(() => {
@@ -89,12 +139,18 @@
 </script>
 
 <svelte:head>
-	{#if domain && enabled}
-		<script data-api={api} data-domain={domain} defer {src}></script>
-		<script>
-			window.plausible ||= function () {
-				(window.plausible.q ||= []).push(arguments);
-			};
-		</script>
+	{#if enabled}
+		{#if domain}
+			<script defer data-api={api} data-domain={domain} {...pageviewProps} {src}></script>
+		{/if}
+		{#if trackErrorPages}
+			<script>
+				window.plausible =
+					window.plausible ||
+					function () {
+						(window.plausible.q = window.plausible.q || []).push(arguments);
+					};
+			</script>
+		{/if}
 	{/if}
 </svelte:head>
