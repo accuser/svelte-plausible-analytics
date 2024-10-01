4c49d0c1cd21772d63b5e5a42c1a5ba93eecf43f
Matthew Gibbons <mhgibbons@me.com>
2024-10-01 06:30:41 +0000

    Export defaults

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,2 @@
-export { default as PlausibleAnalytics } from './PlausibleAnalytics.svelte';
-export { pa } from './store.js';
+export { default as PlausibleAnalytics } from '$lib/components/PlausibleAnalytics.svelte';
+export { default as pa } from './pa.js';
